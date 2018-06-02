c21dd2e7914083b41dedcf7ea0e2c3a4a11cd08a
AniketNavlur <aniket.navlur@gmail.com>
2018-06-02 19:41:41 +0530

    added code for go to goal controller

--- pid.py
+++ pid.py
@@ -2,184 +2,206 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 import time
-from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
+import serial
+from xbee import XBee
 
-ROBOT_MARKER_ID = 7
+UPPER_BOUND_MARKER_ID = 8
+LOWER_BOUND_MARKER_ID = 9
 
-cap = cv2.VideoCapture(0)
+ROBOT_1_MARKER_ID = 11
+ROBOT_1_XBee_ADDR = '\x00\x01'
 
-def nothing(x):
-        pass
 
-# Instantiate an XBee device object.
-def communication(inp):
-	return
-	device = XBeeDevice("COM4", 9600)
-	device.open()
+class XBeeComm:
 
-	# Instantiate a remote XBee device object.
-	remote_device = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A20040F655F0"))
+    def __init__(self, com='COM3', baud=9600):
+        self.xbee = XBee(serial.Serial(com,baud))
+        
+    def tx(self, data, dest='\x00\x01'):
+        self.xbee.tx(dest_addr=dest,data=data)
 
-	# Send data using the remote object.
-	device.send_data_async(remote_device, inp)
-	#time.sleep(0.1)
 
-	device.close()
+class Packets:
 
-def get_marker_angle(point, marker_id):
+    def __init__(self):
+        self.data = []
 
-    theta = None
- 
+    def push(self, data):
+
+        if type(data) is tuple:
+            for i in data:
+                self.push(i)
+            return
+
+        data = str(data)
+        if data.isdigit():
+            data = data + '|'
+
+        self.data.append(data)
+
+    def resetData(self):
+        self.data = []
+
+    def createPacket(self):
+        packet = "<" + "".join(self.data) + ">"
+        return packet
+
+
+class PID:
+
+    def __init__(self):
+        cv2.namedWindow('image')
+
+        cv2.createTrackbar('kp','image',0,500,nothing)
+        cv2.createTrackbar('ki','image',0,500,nothing)
+        cv2.createTrackbar('kd','image',0,500,nothing)
+
+        self.switch = '0 : ON \n1 : OFF'
+        cv2.createTrackbar(self.switch, 'image',0,1,nothing)
+
+        self.kp = cv2.getTrackbarPos('kp','image')
+        self.ki = cv2.getTrackbarPos('ki','image')
+        self.kd = cv2.getTrackbarPos('kd','image')
+        self.CLOSE = cv2.getTrackbarPos(self.switch,'image')
+
+    def setpoint(self, set_point):
+        self.set_point = set_point
+
+    def set_parameters(self):
+        self.kp = cv2.getTrackbarPos('kp','image')
+        self.ki = cv2.getTrackbarPos('ki','image')
+        self.kd = cv2.getTrackbarPos('kd','image')
+        self.CLOSE = cv2.getTrackbarPos(self.switch,'image')
+
+    def get_parameters(self):
+        return self.kp, self.ki, self.kd
+
+def get_aruco_markers(img):
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
+    parameters =  aruco.DetectorParameters_create()
+
+    try:
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+        return corners, ids
+    except:
+        raise Exception("no arucos found")
+
+def get_arena(img):
+    corners, ids= get_aruco_markers(img)
+    ((x1,y1), (x2,y2)) = ((0,0), (0,0))
+    try:
+        upper_corner_pts = get_marker_by_id(corners, ids, UPPER_BOUND_MARKER_ID)
+        lower_corner_pts = get_marker_by_id(corners, ids, LOWER_BOUND_MARKER_ID)
+
+        for i in upper_corner_pts:
+            x1 += int(i[0])
+            y1 += int(i[1])
+
+        for i in lower_corner_pts:
+            x2 += int(i[0])
+            y2 += int(i[1])
+
+        x1 = int(x1/4)
+        x2 = int(x2/4)
+        y1 = int(y1/4)
+        y2 = int(y2/4)
+
+        return img[y1:y2,x1:x2]
+    except:
+        return img
+
+def get_marker_by_id(corners, ids, aruco_id):
+    try:
+        idx = None
+        for i, j in enumerate(ids):
+            if j[0] == aruco_id:
+                idx = i
+        return corners[idx][0]
+    except:
+        raise Exception("aruco with id " + str(aruco_id) + " not found")
+
+def get_marker_angle(frame, point, marker_id):
     while(True):
-        ret, frame = cap.read()
-
-        point = (50, 50)
-        
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
-        parameters =  aruco.DetectorParameters_create()
-        
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-        
         try:
-            idx = None
-            for i, j in enumerate(ids):
-                if j[0] == marker_id:
-                    idx = i
-            #print(idx)
-            corner_point = corners[idx][0]
-            #print(corner_point)
-            #print([int(corner_point[0][0]), int(corner_point[0][1])])
-            Ax = int((corner_point[0][0] + corner_point[3][0])/2)
-            Ay = int((corner_point[0][1] + corner_point[3][1])/2)
+            corners, ids = get_aruco_markers(gray)
+            corner_point = get_marker_by_id(corners, ids, marker_id)
 
-            Bx = int((corner_point[2][0] + corner_point[1][0])/2)
-            By = int((corner_point[2][1] + corner_point[1][1])/2)
+            Tx = int((corner_point[3][0] + corner_point[0][0])/2)
+            Ty = int((corner_point[3][1] + corner_point[0][1])/2)
 
-            cv2.line(gray, point, (Ax, Ay), 255, 1)
-            #cpoints = [int(corner_point[1][0]) - int(corner_point[0][0])]
-            cv2.circle(gray, tuple([corner_point[0][0], 0]), 10, 255, thickness=-1)
-            cv2.circle(gray, (Ax, Ay), 5, 255, thickness=-1)
-            cv2.circle(gray, (Bx, By), 5, 255, thickness=-1)
-            arucolength = np.sqrt((Ax-Bx)**2 + (Ay-By)**2)
-            pointdist_tail = np.sqrt((Ax-point[0])**2 + (Ay-point[1])**2)
-            pointdist_head = np.sqrt((Bx-point[0])**2 + (By-point[1])**2)
-            m2 = (Ay-By)/(Ax-Bx)
-            m1 = (Ay-point[1])/(Ax-point[0])
+            Hx = int((corner_point[2][0] + corner_point[1][0])/2)
+            Hy = int((corner_point[2][1] + corner_point[1][1])/2)
+
+            cv2.line(gray, point, (Tx, Ty), 255, 1)
+            cv2.circle(gray, (Tx, Ty), 5, 255, thickness=-1)
+            cv2.circle(gray, (Hx, Hy), 5, 255, thickness=-1)
+
+            arucolength = np.sqrt((Tx-Hx)**2 + (Ty-Hy)**2)
+            tail_dist = np.sqrt((Tx-point[0])**2 + (Ty-point[1])**2)
+            head_dist = np.sqrt((Hx-point[0])**2 + (Hy-point[1])**2)
+
+            m2 = (Ty-Hy)/(Tx-Hx)
+            m1 = (Ty-point[1])/(Tx-point[0])
+
             theta = np.arctan((m2-m1)/(1+m1*m2))*180/np.pi
-            
-            if pointdist_head > np.sqrt(arucolength**2 + pointdist_tail**2):
+
+            if head_dist > np.sqrt(arucolength**2 + tail_dist**2):
                 if theta < 0:
                     theta = 180 + theta
                 elif theta >0:
                     theta = -180 + theta
                 else:
                     theta = 180
-                
-            
-            #print("theta = ", theta)
-            #break
-        except Exception as e:
-            print(e)
-            pass
-     
-        gray = aruco.drawDetectedMarkers(gray, corners)
-        
-        cv2.imshow('frame',gray)
+
+            gray = aruco.drawDetectedMarkers(gray, corners)
+        except:
+            Tx,Ty,Hx,Hy,theta= (0,0,0,0,0)
+
+        cv2.imshow('image',gray)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
         break
 
-    if theta is not None:
-        return theta
+    return Tx,Ty,Hx,Hy,int(theta)
 
-class PID():
-
-	def __init__(self):
-		cv2.namedWindow('image')
-
-		self.sample_time = 0
-		self.time_now = time.time()
-		self.time_previous = self.time_now
-
-		self.set_point = (0, 0)
-
-		self.P = 0.00
-		self.I = 0.00
-		self.D = 0.00
-		self.error_previous = 0.0
-
-		cv2.createTrackbar('kp','image',0,255,nothing)
-		cv2.createTrackbar('ki','image',0,255,nothing)
-		cv2.createTrackbar('kd','image',0,255,nothing)
-		cv2.createTrackbar('sample_time','image',0,255,nothing)
-		self.switch = '0 : ON \n1 : OFF'
-		cv2.createTrackbar(self.switch, 'image',0,1,nothing)
-
-	def setpoint(self, set_point):
-		self.set_point = set_point
-
-	def set_parameters(self):
-
-		self.kp = 0.1 * cv2.getTrackbarPos('kp','image')
-		self.ki = 0.1 * cv2.getTrackbarPos('ki','image')
-		self.kd = 0.1 * cv2.getTrackbarPos('ki','image')
-		self.sample_time = cv2.getTrackbarPos('sample_time','image')
-		self.CLOSE = cv2.getTrackbarPos(self.switch,'image')
+def nothing(x):
+    pass
 
 
-	def get_parameters(self):
-		return (self.kp, self.ki, self.kd, self.sample_time)
+cap = cv2.VideoCapture('/dev/video1')
 
-	def compute(self):
-
-
-		self.set_parameters()
-		print(self.get_parameters())
-
-		error_now = None
-		while error_now is None:
-			error_now = get_marker_angle(self.set_point, 8)
-
-		de = error_now - self.error_previous
-
-		self.time_now = time.time()
-		dt = self.time_now - self.time_previous
-
-		if dt < self.sample_time:
-			return None
-
-		self.time_previous = self.time_now
-
-		self.P = self.kp * error_now
-		self.I += self.ki * error_now * dt
-		self.D = self.kd * de / dt
-
-		output = self.P + self.I + self.D
-
-		if output > 124:
-			output = 124
-			#self.I = output
-
-		if output < -124:
-			output = -124
-			#self.I = output
-
-		self.error_previous = error_now
-		print(output)
-
-		return output
+dest_x = int(input("target_x="))
+dest_y = int(input("target_y="))
+point=(dest_x, dest_y)
 
 pid = PID()
+comm = XBeeComm()
+packet = Packets()
 
 while(1):
-	data = None
-	while(data is None):
-		data = pid.compute()
-	communication(chr(int(data+125)))
+    ret, frame = cap.read()
+    frame = get_arena(frame)
 
-	if pid.CLOSE != 0:
-		break
+    Tx,Ty,Hx,Hy,theta = get_marker_angle(frame, point,ROBOT_1_MARKER_ID)
 
+    pid.set_parameters()
+
+    packet.push(('T', dest_x, dest_y))
+    packet.push(('P', pid.kp, pid.ki, pid.kd))
+    packet.push(('R', Hx, Hy, Tx, Ty))
+    packet.push(('A', theta+180))
+
+    packet_data = packet.createPacket()
+
+    comm.tx(packet_data)
+
+    packet.resetData()
+
+    if pid.CLOSE != 0:
+            break
+        
 cap.release()
 cv2.destroyAllWindows()
